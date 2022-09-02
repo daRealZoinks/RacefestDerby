3f263f6b3e19696cdc528c329f886c4e50b17f4e
Zoinks <murarashuvlad@gmail.com>
2022-09-03 00:12:52 +0300

    turned the visual script into c# code

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [SerializeField] private float motorForce = 1000.0f;
+    [SerializeField] private float brakeForce = 3000.0f;
+    [SerializeField] private float maximumSteeringAngle = 30.0f;
+
+    [SerializeField] private WheelCollider frontRightWheelCollider;
+    [SerializeField] private WheelCollider frontLeftWheelCollider;
+    [SerializeField] private WheelCollider rearRightWheelCollider;
+    [SerializeField] private WheelCollider rearLeftWheelCollider;
+
+    [SerializeField] private Transform frontRightWheelTransform;
+    [SerializeField] private Transform frontLeftWheelTransform;
+    [SerializeField] private Transform rearRightWheelTransform;
+    [SerializeField] private Transform rearLeftWheelTransform;
+
+    private float _currentMotorForce;
+    public float CurrentMotorForce
+    {
+        set
+        {
+            _currentMotorForce = value;
+        }
+    }
+
+    private float _currentBrakeForce;
+    public float CurentBreakForce
+    {
+        set
+        {
+            _currentBrakeForce = value;
+        }
+    }
+
+    private float _currentSteerAngle;
+    public float CurrentSteerAngle
+    {
+        set
+        {
+            _currentSteerAngle = value;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMotor(_currentMotorForce);
+        HandleBrake(_currentBrakeForce);
+        HandleSteering(_currentSteerAngle);
+    }
+
+    private void Update()
+    {
+        UpdateWheel(frontLeftWheelCollider, frontLeftWheelTransform);
+        UpdateWheel(frontRightWheelCollider, frontRightWheelTransform);
+        UpdateWheel(rearLeftWheelCollider, rearLeftWheelTransform);
+        UpdateWheel(rearRightWheelCollider, rearRightWheelTransform);
+    }
+
+
+    void HandleMotor(float motorTorque)
+    {
+        frontRightWheelCollider.motorTorque = motorTorque;
+        frontLeftWheelCollider.motorTorque = motorTorque;
+    }
+
+    void HandleBrake(float brakeTorque)
+    {
+        frontRightWheelCollider.brakeTorque = brakeTorque;
+        frontLeftWheelCollider.brakeTorque = brakeTorque;
+        rearRightWheelCollider.brakeTorque = brakeTorque;
+        rearLeftWheelCollider.brakeTorque = brakeTorque;
+    }
+
+    void HandleSteering(float steerAngle)
+    {
+        frontRightWheelCollider.steerAngle = steerAngle;
+        frontLeftWheelCollider.steerAngle = steerAngle;
+    }
+
+    void UpdateWheel(WheelCollider wheelCollider, Transform wheelTrasform)
+    {
+        wheelCollider.GetWorldPose(out var position, out var rotation);
+        wheelTrasform.SetPositionAndRotation(position, rotation);
+    }
+}
